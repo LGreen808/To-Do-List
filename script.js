8c010da13a24a0bed53422de4673cca5f86269af
LGreen808 <526LGreen@frhsd.com>
2025-03-28 12:34:05 +0000

    Got the enter key to work!

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ if (weekDay === 'Monday') {
 let tasks = []
 
 //Add an event listener to add tasks to the add task btn
-document.getElementById('addTaskBtn').addEventListener('click' function() {
+document.getElementById('addTaskBtn').addEventListener('click', function() {
     //Storing text-value from input box as a variable 'taskInput'
     let taskInput = document.getElementById('taskInput').value
 
@@ -27,7 +27,23 @@ document.getElementById('addTaskBtn').addEventListener('click' function() {
      displayTasks()
     }
 
-}) document.getElementById('addTaskBtn')
+})
+window.addEventListener('keydown', function(event) {
+    if (event.key == "Enter") {
+    //Storing text-value from input box as a variable 'taskInput'
+    let taskInput = document.getElementById('taskInput').value
+
+    //This line of code below is the if statement checking if there is any input in the text-box to enter in tasks
+    if (taskInput){ 
+    //This line of code below pushes the id of task input into this if statement
+    tasks.push(taskInput) 
+    //This line of code below clears the text-box to input the tasks after you press the Add-Task Button
+    document.getElementById('taskInput').value = ''
+    //This line of code below is going to display my tasks on my to-do list
+     displayTasks()
+    }
+    }
+})
 
 
 // Function to display the tasks from tasks[] in the Unordered List below
